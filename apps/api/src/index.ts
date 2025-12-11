1cb1cbc1fac351c1ac9bb080bef809c54ec798a6
Sahil Nagel <sahilnagel@gmail.com>
2025-12-11 20:29:41 +0530

    added the moraco-code editor in the website

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -20,6 +20,20 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room ${roomId}`);
+  });
+
+  //share code with everyone in room
+  socket.on("code-change", ({ roomId, code }) => {
+    socket.to(roomId).emit("code-update", code);
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+
 });
 
 const PORT = process.env.PORT || 4000;
